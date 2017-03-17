18a35cec8868e3f4126e5a0d4b2d2120d2cfb553
Carlos R. Ueda <carlosueda@mti.mt.gov.br>
2017-03-17 16:12:40 -0400

    new method in usuario.service.ts

--- src/app/usuario/usuario.service.ts
+++ src/app/usuario/usuario.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Http, Response, Jsonp } from '@angular/http';
+import { Http, Response, Jsonp, RequestOptions, Headers  } from '@angular/http';
 
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/catch';
@@ -26,6 +26,15 @@ export class UsuarioService {
 						.catch(this.handleError);
 	}
 	
+	updateUsuario(): Observable<string>{
+		let headers = new Headers({ 'Content-Type': 'application/json' });
+		let options = new RequestOptions({ headers: headers });
+		return this.http.post('http://localhost:8080/GRRecurso/public/api/rest/usuario/update', 
+		{}, 
+		options)
+		.map(this.extractData)
+		.catch(this.handleError);
+	}
 	
 	private extractData(res: Response){
 		let body = res.json();		
